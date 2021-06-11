186a6140c156cd894a87da2df01fe9863f2e5f69
Rakesh Thatiparthi <53700572+rakeshthatiparthi@users.noreply.github.com>
2021-06-11 23:18:17 +0530

    Update Case Study #1 - Danny's Diner.sql

--- Case Study #1 - Danny's Diner.sql
+++ Case Study #1 - Danny's Diner.sql
@@ -241,4 +241,89 @@ GROUP BY me.customer_id;
 --  customer_id | points_earned_in_first_week 
 -- -------------+----------------------------
 --  B           |   440
---  A           |    1020
+--  A           |   1020
+
+
+-- Bonus Questions
+
+-- Join all things
+
+SELECT
+	s.customer_id,
+	s.order_date,
+    	m.product_name,
+    	m.price,
+    	CASE
+		WHEN s.order_date < me.join_date THEN 'N'
+       		WHEN me.join_date is null THEN 'N'
+        	ELSE 'Y'
+	END as member
+FROM sales s
+JOIN menu m
+	ON s.product_id = m.product_id
+LEFT JOIN members me
+	ON s.customer_id = me.customer_id;
+	
+
+-- Output
+
+--  customer_id | order_date | product_name | price | member 
+-- -------------+------------+--------------+-------+--------
+--  A           | 2021-01-01 | sushi        |    10 | N
+--  A           | 2021-01-01 | curry        |    15 | N
+--  A           | 2021-01-07 | curry        |    15 | Y
+--  A           | 2021-01-10 | ramen        |    12 | Y
+--  A           | 2021-01-11 | ramen        |    12 | Y
+--  A           | 2021-01-11 | ramen        |    12 | Y
+--  B           | 2021-01-01 | curry        |    15 | N
+--  B           | 2021-01-02 | curry        |    15 | N
+--  B           | 2021-01-04 | sushi        |    10 | N
+--  B           | 2021-01-11 | sushi        |    10 | Y
+--  B           | 2021-01-16 | ramen        |    12 | Y
+--  B           | 2021-02-01 | ramen        |    12 | Y
+--  C           | 2021-01-01 | ramen        |    12 | N
+--  C           | 2021-01-01 | ramen        |    12 | N
+--  C           | 2021-01-07 | ramen        |    12 | N
+
+
+-- Rank All The Things
+WITH cte as(
+	SELECT
+		s.customer_id,
+    		s.order_date,
+    		m.product_name,
+    		m.price,
+    		CASE
+			WHEN s.order_date < me.join_date THEN 'N'
+        		WHEN me.join_date is null THEN 'N'
+        		ELSE 'Y'
+		END as member
+	FROM sales s
+	JOIN menu m
+		ON s.product_id = m.product_id
+	LEFT JOIN members me
+		ON s.customer_id = me.customer_id)
+    SELECT
+    *,
+    CASE WHEN member = 'Y' THEN rank() OVER (PARTITION BY customer_id, member ORDER BY order_date) ELSE NULL END as ranking
+    FROM cte
+    
+    -- Output
+
+--  customer_id | order_date | product_name | price | member | ranking 
+-- -------------+------------+--------------+-------+--------+---------
+--  A           | 2021-01-01 | sushi        |    10 | N      |        
+--  A           | 2021-01-01 | curry        |    15 | N      |        
+--  A           | 2021-01-07 | curry        |    15 | Y      |       1
+--  A           | 2021-01-10 | ramen        |    12 | Y      |       2
+--  A           | 2021-01-11 | ramen        |    12 | Y      |       3
+--  A           | 2021-01-11 | ramen        |    12 | Y      |       3
+--  B           | 2021-01-01 | curry        |    15 | N      |        
+--  B           | 2021-01-02 | curry        |    15 | N      |        
+--  B           | 2021-01-04 | sushi        |    10 | N      |        
+--  B           | 2021-01-11 | sushi        |    10 | Y      |       1
+--  B           | 2021-01-16 | ramen        |    12 | Y      |       2
+--  B           | 2021-02-01 | ramen        |    12 | Y      |       3
+--  C           | 2021-01-01 | ramen        |    12 | N      |        
+--  C           | 2021-01-01 | ramen        |    12 | N      |        
+--  C           | 2021-01-07 | ramen        |    12 | N      |       
